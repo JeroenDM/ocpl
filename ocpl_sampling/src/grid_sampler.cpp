ca797dc0998f5c6c99394b4240b915dfd2423267
JeroenDM <jeroendemaeyer@live.be>
2021-01-07 17:26:59 +0100

    add extra input check to grid sampler constructor

--- ocpl_sampling/src/grid_sampler.cpp
+++ ocpl_sampling/src/grid_sampler.cpp
@@ -7,67 +7,70 @@ namespace ocpl
 {
 std::vector<double> GridSampler::range(double lower_bound, double upper_bound, int num_samples)
 {
-  std::vector<double> range;
-  if (num_samples == 1)
-  {
-    range = { lower_bound };
-  }
-  else
-  {
-    range.resize(num_samples);
-    double increment = (upper_bound - lower_bound) / (num_samples - 1);
-    for (int i = 0; i < num_samples; ++i)
+    std::vector<double> range;
+    if (num_samples == 1)
     {
-      range[i] = lower_bound + i * increment;
+        range = { lower_bound };
     }
-  }
-  return range;
+    else
+    {
+        range.resize(num_samples);
+        double increment = (upper_bound - lower_bound) / (num_samples - 1);
+        for (int i = 0; i < num_samples; ++i)
+        {
+            range[i] = lower_bound + i * increment;
+        }
+    }
+    return range;
 }
 
 void GridSampler::recursiveGridSampling(int index, std::vector<double> prev_values,
                                         std::vector<std::vector<double>>& grid)
 {
-  auto sample_range = range(lower_bounds_[index], upper_bounds_[index], num_samples_[index]);
-  if (index < (dimensions_ - 1))
-  {
-    for (auto value : sample_range)
+    auto sample_range = range(lower_bounds_[index], upper_bounds_[index], num_samples_[index]);
+    if (index < (dimensions_ - 1))
     {
-      std::vector<double> pvi(prev_values);
-      pvi.push_back(value);
-      recursiveGridSampling(index + 1, pvi, grid);
+        for (auto value : sample_range)
+        {
+            std::vector<double> pvi(prev_values);
+            pvi.push_back(value);
+            recursiveGridSampling(index + 1, pvi, grid);
+        }
     }
-  }
-  else
-  {
-    for (auto value : sample_range)
+    else
     {
-      // get a new instance of the previous values every iteration in this loop
-      std::vector<double> pvi(prev_values);
-      pvi.push_back(value);
-      grid.push_back(pvi);
+        for (auto value : sample_range)
+        {
+            // get a new instance of the previous values every iteration in this loop
+            std::vector<double> pvi(prev_values);
+            pvi.push_back(value);
+            grid.push_back(pvi);
+        }
     }
-  }
 }
 
 std::vector<std::vector<double>> GridSampler::getSamples(const int /* n */)
 {
-  std::vector<std::vector<double>> grid;
-  std::vector<double> empty_vector;
-  recursiveGridSampling(0, empty_vector, grid);
+    std::vector<std::vector<double>> grid;
+    std::vector<double> empty_vector;
+    recursiveGridSampling(0, empty_vector, grid);
 
-  return grid;
+    return grid;
 }
 
 void GridSampler::addDimension(double lower_bound, double upper_bound)
 {
-  throw std::runtime_error("You should supply a number of samples for dimensions when creating grid samplers.");
+    throw std::runtime_error("You should supply a number of samples for dimensions when creating grid samplers.");
 }
 
 void GridSampler::addDimension(double lower_bound, double upper_bound, int num_samples)
 {
-  Sampler::addDimension(lower_bound, upper_bound);
-  if (num_samples == 1 and lower_bound != upper_bound)
-    throw std::invalid_argument("Lower bound should equal upper bound if number of samples is 1.");
-  num_samples_.push_back(num_samples);
+    Sampler::addDimension(lower_bound, upper_bound);
+
+    if (num_samples == 0)
+        throw std::invalid_argument("The number of samples for a dimension cannot be zero, it must be at least 1.");
+    if (num_samples == 1 and lower_bound != upper_bound)
+        throw std::invalid_argument("Lower bound should equal upper bound if number of samples is 1.");
+    num_samples_.push_back(num_samples);
 }
 }  // namespace ocpl
