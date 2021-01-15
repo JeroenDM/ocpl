ba4461532a71ec95023acf3ea2b702e7f780456b
JeroenDM <jeroendemaeyer@live.be>
2021-01-15 09:29:39 +0100

    add test data with string literal

--- ocpl_planning/test/test_io.cpp
+++ ocpl_planning/test/test_io.cpp
@@ -1,6 +1,31 @@
 #include <ocpl_planning/io.h>
 
+#include <string>
 #include <gtest/gtest.h>
+
+const std::string L_PROFILE {R"irl(
+variables
+config home 0 -1.5708 1.5708 0 0 0
+pose work 0.8 -0.5 0 0 0 0
+set-reference work
+pose P1 0.18 0 0.02 0 135 90
+pose P2 0.18 1 0.02 0 135 90
+pose Papp 0.16 0 0.04 0 135 90
+pose Pret 0.16 1 0.04 0 135 90
+set-reference default
+
+commands
+movej home
+movep Papp constraints C1
+movel P1 constraints C1 approach 0 0 0.1
+movel P2 constraints C1 retract 0 0 -0.1
+movel Pret constraints C1
+movej home
+
+constraints
+rpy C1 0 0 3.1415 0 0 3.1415
+)irl"};
+
 
 TEST(TestFileParser, TestParseVariable)
 {
