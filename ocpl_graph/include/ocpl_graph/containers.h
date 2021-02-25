f69ca3b7b400ce8eacda80f8de4ff5329111e957
JeroenDM <jeroendemaeyer@live.be>
2021-02-25 15:47:02 +0100

    add cleaner implementation of generic containers

---
+++ ocpl_graph/include/ocpl_graph/containers.h
@@ -0,0 +1,199 @@
+#pragma once
+
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <functional>
+
+namespace ocpl
+{
+/** \brief Generic interface around containers in standard library. **/
+template <typename T>
+class BaseContainer
+{
+  public:
+    virtual ~BaseContainer() = default;
+    virtual T pop() = 0;
+    virtual void push(const T& v, const size_t waypoint) = 0;
+    virtual bool empty() = 0;
+};
+
+/** \brief Last in first out container. **/
+template <typename T>
+class StackContainer : public BaseContainer<T>
+{
+    std::vector<T> data;
+
+  public:
+    StackContainer() = default;
+    ~StackContainer() = default;
+    virtual T pop() override
+    {
+        T v = data.back();
+        data.pop_back();
+        return v;
+    }
+    virtual void push(const T& v, const size_t /* waypoint */) override
+    {
+        data.emplace_back(v);
+    }
+    virtual bool empty() override
+    {
+        return data.empty();
+    }
+};
+
+/** \brief First in first out container. **/
+template <typename T>
+class QueueContainer : public BaseContainer<T>
+{
+    std::deque<T> data;
+
+  public:
+    QueueContainer() = default;
+    ~QueueContainer() = default;
+    virtual T pop() override
+    {
+        T v = data.front();
+        data.pop_front();
+        return v;
+    }
+    virtual void push(const T& v, const size_t /* waypoint */) override
+    {
+        data.emplace_back(v);
+    }
+    virtual bool empty() override
+    {
+        return data.empty();
+    }
+};
+
+template <typename T>
+class PriorityStackContainer : public BaseContainer<T>
+{
+    using compare_fun_t = std::function<bool(const T&, const T&)>;
+    using priority_queue_t = std::priority_queue<T, std::vector<T>, compare_fun_t>;
+
+    size_t max_waypoints_;
+    std::vector<priority_queue_t> data_;
+
+    size_t current_waypoint_{ 0 };
+
+  public:
+    PriorityStackContainer(size_t max_waypoints, compare_fun_t compare_fun) : max_waypoints_(max_waypoints)
+    {
+        for (size_t k{ 0 }; k < max_waypoints_; ++k)
+            data_.emplace_back(priority_queue_t{ compare_fun });
+    }
+
+    ~PriorityStackContainer() = default;
+
+    virtual T pop() override
+    {
+        if (data_[current_waypoint_].empty())
+        {
+            if (current_waypoint_ > 0)
+            {
+                current_waypoint_--;
+                return pop();
+            }
+            else
+            {
+                throw std::out_of_range("Trying to pop an element from an empty PriorityStackContainer");
+            }
+        }
+        else
+        {
+            T v = data_[current_waypoint_].top();
+            data_[current_waypoint_].pop();
+            return v;
+        }
+    }
+
+    virtual void push(const T& vertice, const size_t waypoint) override
+    {
+        if (waypoint < max_waypoints_)
+        {
+            data_[waypoint].emplace(vertice);
+            if (waypoint > current_waypoint_)
+            {
+                current_waypoint_ = waypoint;
+            }
+        }
+        else
+        {
+            throw std::out_of_range("Trying to push an element past the waypoint size of this PriorityQueueContainer");
+        }
+    }
+
+    virtual bool empty() override
+    {
+        // Everyting above the current waypoint is empty by definition, only check below
+        return std::all_of(data_.begin(), data_.begin() + current_waypoint_, [](const auto& q) { return q.empty(); });
+    }
+};
+
+template <typename T>
+class PriorityQueueContainer : public BaseContainer<T>
+{
+    using compare_fun_t = std::function<bool(const T&, const T&)>;
+    using priority_queue_t = std::priority_queue<T, std::vector<T>, compare_fun_t>;
+
+    size_t max_waypoints_;
+    std::vector<priority_queue_t> data_;
+
+    size_t current_waypoint_{ 0 };
+
+  public:
+    PriorityQueueContainer(size_t max_waypoints, compare_fun_t compare_fun) : max_waypoints_(max_waypoints)
+    {
+        for (size_t k{ 0 }; k < max_waypoints_; ++k)
+            data_.emplace_back(priority_queue_t{ compare_fun });
+    }
+
+    ~PriorityQueueContainer() = default;
+
+    virtual T pop() override
+    {
+        if (data_[current_waypoint_].empty())
+        {
+            if (current_waypoint_ < max_waypoints_ - 1)
+            {
+                current_waypoint_++;
+                return pop();
+            }
+            else
+            {
+                throw std::out_of_range("Trying to pop an element from an empty PriorityQueueContainer");
+            }
+        }
+        else
+        {
+            T v = data_[current_waypoint_].top();
+            data_[current_waypoint_].pop();
+            return v;
+        }
+    }
+    virtual void push(const T& v, const size_t waypoint) override
+    {
+        if (waypoint < max_waypoints_)
+        {
+            data_[waypoint].push(v);
+            // potentially move the pop waypoint back
+            if (waypoint < current_waypoint_)
+            {
+                current_waypoint_ = waypoint;
+            }
+        }
+        else
+        {
+            throw std::out_of_range("Trying to push an element past the waypoint size of this PriorityQueueContainer");
+        }
+    }
+    virtual bool empty() override
+    {
+        // Everyting below the current waypoint is empty by definition, only check above
+        return std::all_of(data_.begin() + current_waypoint_, data_.end(), [](const auto& q) { return q.empty(); });
+    }
+};
+}  // namespace ocpl
---
+++ ocpl_graph/test/test_graph.cpp
@@ -0,0 +1,137 @@
+#include <ocpl_graph/containers.h>
+#include <ocpl_graph/graph.h>
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
+
+using namespace ocpl;
+
+TEST(TestContainers, TestStack)
+{
+    StackContainer<int> stack;
+    stack.push(3, 0);
+    stack.push(5, 0);
+
+    EXPECT_FALSE(stack.empty());
+    EXPECT_EQ(stack.pop(), 5);
+
+    stack.push(2, 0);
+
+    EXPECT_EQ(stack.pop(), 2);
+    EXPECT_EQ(stack.pop(), 3);
+    EXPECT_TRUE(stack.empty());
+}
+
+TEST(TestContainers, TestQueue)
+{
+    QueueContainer<int> queue;
+    queue.push(3, 0);
+    queue.push(5, 0);
+
+    EXPECT_FALSE(queue.empty());
+    EXPECT_EQ(queue.pop(), 3);
+
+    queue.push(2, 0);
+
+    EXPECT_EQ(queue.pop(), 5);
+    EXPECT_EQ(queue.pop(), 2);
+    EXPECT_TRUE(queue.empty());
+}
+
+TEST(TestContainers, TestPriorityStack)
+{
+    // we expect the smallest values are popped first
+    // and in addition the values of the last waypoint that contains values
+    // are popped first
+    PriorityStackContainer<int> ps(3, std::greater<int>());
+    ps.push(3, 0);
+    ps.push(3, 0);
+    ps.push(5, 0);
+    ps.push(8, 1);
+    ps.push(9, 1);
+
+    EXPECT_FALSE(ps.empty());
+
+    EXPECT_EQ(ps.pop(), 8);
+    EXPECT_EQ(ps.pop(), 9);
+
+    EXPECT_EQ(ps.pop(), 3);
+
+    ps.push(9, 1);
+    ps.push(2, 0);
+
+    EXPECT_EQ(ps.pop(), 9);
+    EXPECT_EQ(ps.pop(), 2);
+    EXPECT_EQ(ps.pop(), 3);
+    EXPECT_EQ(ps.pop(), 5);
+    EXPECT_TRUE(ps.empty());
+
+    ps.push(7, 1);
+    EXPECT_EQ(ps.pop(), 7);
+    EXPECT_TRUE(ps.empty());
+}
+
+TEST(TestContainers, TestPriorityQueue)
+{
+    // we expect the smallest values are popped first
+    // and in addition the values of first waypoint that contains values
+    // to pop first
+    PriorityQueueContainer<int> pq(3, std::greater<int>());
+    pq.push(3, 0);
+    pq.push(5, 0);
+    pq.push(8, 1);
+    pq.push(9, 1);
+
+    EXPECT_FALSE(pq.empty());
+
+    EXPECT_EQ(pq.pop(), 3);
+    EXPECT_EQ(pq.pop(), 5);
+    EXPECT_EQ(pq.pop(), 8);
+
+    pq.push(7, 1);
+    pq.push(2, 0);
+
+    EXPECT_EQ(pq.pop(), 2);
+    EXPECT_EQ(pq.pop(), 7);
+    EXPECT_EQ(pq.pop(), 9);
+    EXPECT_TRUE(pq.empty());
+}
+
+TEST(TestContainers, TestWithDoubleVector)
+{
+    using Vectord = std::vector<double>;
+    PriorityStackContainer<Vectord> ps(3, [](const Vectord& a, const Vectord& b) { return a[0] > b[0]; });
+
+    ps.push({ 3, 8 }, 0);
+    ps.push({ 3, 7 }, 0);
+    ps.push({ 5, 6 }, 0);
+    ps.push({ 8, 5 }, 1);
+    ps.push({ 9, 4 }, 1);
+
+    EXPECT_FALSE(ps.empty());
+
+    EXPECT_EQ(ps.pop().at(0), 8);
+    EXPECT_EQ(ps.pop().at(0), 9);
+
+    EXPECT_EQ(ps.pop().at(0), 3);
+
+    ps.push({ 9, 8 }, 1);
+    ps.push({ 2, 8 }, 0);
+
+    EXPECT_EQ(ps.pop().at(0), 9);
+    EXPECT_EQ(ps.pop().at(0), 2);
+    EXPECT_EQ(ps.pop().at(0), 3);
+    EXPECT_EQ(ps.pop().at(0), 5);
+    EXPECT_TRUE(ps.empty());
+
+    ps.push({ 7, 99 }, 1);
+    EXPECT_EQ(ps.pop().at(0), 7);
+    EXPECT_TRUE(ps.empty());
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
